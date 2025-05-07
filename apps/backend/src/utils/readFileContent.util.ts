ed02d23c1a43e3e81e0078993598b9c783fe10cf
Renan Gomes <renango2015@gmail.com>
2025-05-07 01:30:08 -0300

    fix(readFileContent): resolve incorrect file path handling in 'readFileSync'

--- apps/backend/src/utils/readFileContent.util.ts
+++ apps/backend/src/utils/readFileContent.util.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 export function readFileContent(filePath: string) {
   try {
-    const content = readFileSync(join(__dirname, filePath), 'utf-8').trim();
+    const content = readFileSync(join(process.cwd(), filePath), 'utf-8').trim();
     if (!content) throw new Error(`${filePath} file is empty or invalid`);
     return content;
   } catch (err) {
