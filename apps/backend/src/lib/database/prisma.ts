557394996746285910865848f031ca754a8ad02a
Renan Gomes <renango2015@gmail.com>
2025-06-01 13:14:06 -0300

    chore(db): minor changes

--- apps/backend/src/lib/database/prisma.ts
+++ apps/backend/src/lib/database/prisma.ts
@@ -5,7 +5,7 @@ declare global {
   var prisma: ReturnType<typeof prismaSingleton>;
 }
 
-const prismaSingleton = () => new PrismaClient({ datasourceUrl: ENV.POSTGRES_DATABASE_URL });
+const prismaSingleton = () => new PrismaClient();
 
 export const prisma = globalThis.prisma ?? prismaSingleton();
 if (ENV.NODE_ENV !== 'production') globalThis.prisma = prisma;
