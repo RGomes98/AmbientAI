6f1c43f4b274c6f951213495270895d6a4ee6f70
Renan Tavares <renan.tavares@mettainnovations.com.br>
2025-09-03 07:31:54 -0300

    fix(api): address issues related to typescript build

--- apps/backend/src/controllers/air-quality.controller.ts
+++ apps/backend/src/controllers/air-quality.controller.ts
@@ -1,17 +1,13 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
 
-import type { AirQualityCreate } from '../domain/air-quality/air-quality.type';
 import type { AirQualityService } from '../services/air-quality.service';
-import type { ApiKeyHeaders } from '../domain/api-key/api-key.type';
+import { AirQualitySchema } from '../domain/air-quality/air-quality.schema';
 
 export class AirQualityController {
   constructor(private service: AirQualityService) {}
 
-  public async create(
-    request: FastifyRequest<{ Headers: ApiKeyHeaders; Body: AirQualityCreate[] }>,
-    reply: FastifyReply
-  ) {
-    const data = await this.service.createAirQualityEntries(request.body);
+  public async create(request: FastifyRequest, reply: FastifyReply) {
+    const data = await this.service.createAirQualityEntries(AirQualitySchema.array().parse(request.body));
     return reply.status(201).send(data);
   }
 
--- apps/backend/src/guards/api-key.guard.ts
+++ apps/backend/src/guards/api-key.guard.ts
@@ -10,7 +10,7 @@ const apiKeyRepository = new ApiKeyRepository(prisma);
 const apiKeyService = new ApiKeyService(apiKeyRepository);
 
 export class ApiKeyGuard {
-  public static async verify(request: FastifyRequest<{ Headers: ApiKeyHeaders }>) {
+  public static async verify(request: FastifyRequest) {
     const plainTextApiKey = ApiKeyValueObject.validateStructure(request.headers['x-api-key']);
     const sessionPayload = await apiKeyService.createSessionFromApiKey(plainTextApiKey);
     request.user = sessionPayload;
