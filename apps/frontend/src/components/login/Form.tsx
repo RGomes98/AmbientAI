f8a84bb22a58d799104faa77ff3f2b3e28593e6b
Renan Gomes <renango2015@gmail.com>
2025-09-07 08:16:26 -0300

    fix(login): add 'POST' method to HTML form
    Prevents password and username from being displayed in the URL

--- apps/frontend/src/components/login/Form.tsx
+++ apps/frontend/src/components/login/Form.tsx
@@ -9,7 +9,7 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'form'>)
   const { isLoading, handleLogin } = useLogin();
 
   return (
-    <form onSubmit={handleLogin} className={cn('flex flex-col gap-6', className)} {...props}>
+    <form method='POST' onSubmit={handleLogin} className={cn('flex flex-col gap-6', className)} {...props}>
       <div className='flex flex-col items-center gap-2 text-center'>
         <h1 className='text-3xl font-bold'>AmbientAI</h1>
         <p className='text-muted-foreground text-sm text-balance'>
