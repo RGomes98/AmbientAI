5fb07d2ee832b9e4af53e4b34bc15b6131e03480
Renan Gomes <renango2015@gmail.com>
2025-09-27 08:36:59 -0300

    feat(frontend): layout improvements

--- apps/frontend/src/components/dashboard/chart-area-interactive.tsx
+++ apps/frontend/src/components/dashboard/chart-area-interactive.tsx
@@ -7,35 +7,67 @@ import { useState } from 'react';
 import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { AirQuality } from '@/lib/schemas/air-quality.schema';
 import { Query } from '@/utils/query.util';
 import { useMobile } from '@/hooks/shared/useMobile.hook';
 import { DayRangeQueryParamSchema } from '@/lib/schemas/query.schema';
 
-const chartConfig = {
-  value: {
-    label: 'Poluentes Orgânicos (Índice):\u00A0\u00A0',
-    color: 'var(--primary)',
+const metrics = {
+  tvocIndex: {
+    heading: { widescreen: 'Compostos Orgânicos Voláteis (TVOC)', mobile: 'TVOC' },
+    description: { long: 'Níveis de compostos orgânicos voláteis', short: 'TVOC' },
   },
-} satisfies ChartConfig;
+  rco2: {
+    heading: { widescreen: 'Dióxido de Carbono (CO₂)', mobile: 'CO₂' },
+    description: { long: 'Níveis de dióxido de carbono no ambiente', short: 'CO₂' },
+  },
+  noxIndex: {
+    heading: { widescreen: 'Óxidos de Nitrogênio (NOx)', mobile: 'NOx' },
+    description: { long: 'Níveis de óxidos de nitrogênio no ar', short: 'NOx' },
+  },
+  pm01Standard: {
+    heading: { widescreen: 'Material Particulado PM1.0', mobile: 'PM1.0' },
+    description: { long: 'Concentração de partículas PM1.0 no ar', short: 'PM1.0' },
+  },
+  pm02Standard: {
+    heading: { widescreen: 'Material Particulado PM2.5', mobile: 'PM2.5' },
+    description: { long: 'Concentração de partículas PM2.5 no ar', short: 'PM2.5' },
+  },
+  pm10Standard: {
+    heading: { widescreen: 'Material Particulado PM10', mobile: 'PM10' },
+    description: { long: 'Concentração de partículas PM10 no ar', short: 'PM10' },
+  },
+  atmpCompensated: {
+    heading: { widescreen: 'Temperatura do Ar', mobile: 'Temperatura' },
+    description: { long: 'Temperatura do ar registrada no ambiente', short: 'Temperatura' },
+  },
+  rhumCompensated: {
+    heading: { widescreen: 'Umidade Relativa do Ar', mobile: 'Umidade' },
+    description: { long: 'Umidade relativa do ar registrada', short: 'Umidade' },
+  },
+};
 
 const timeRangeDescriptions: { [key: string]: { long: string; short: string } } = {
-  '14d': { long: 'Níveis de poluentes orgânicos dos últimos 14 dias', short: 'Últimos 14 dias' },
-  '7d': { long: 'Níveis de poluentes orgânicos dos últimos 7 dias', short: 'Últimos 7 dias' },
-  '1d': { long: 'Níveis de poluentes orgânicos nas últimas 24 horas', short: 'Últimas 24 horas' },
+  '14d': { long: 'nos últimos 14 dias', short: 'Últimos 14 dias' },
+  '7d': { long: 'nos últimos 7 dias', short: 'Últimos 7 dias' },
+  '1d': { long: 'nas últimas 24 horas', short: 'Últimas 24 horas' },
 };
+
+type Metric = keyof typeof metrics;
 
 export function ChartAreaInteractive({ data }: { data: AirQuality[] }) {
   const searchParams = useSearchParams();
   const initialRange = DayRangeQueryParamSchema.parse(searchParams.get('range'));
 
+  const [selectedMetric, setSelectedMetric] = useState<Metric>('atmpCompensated');
   const [timeRange, setTimeRange] = useState<string>(initialRange);
   const isMobile = useMobile();
   const router = useRouter();
 
-  const currentDescription = timeRangeDescriptions[timeRange];
-  const chartData = data.toReversed().map((data) => ({ value: data.tvocIndex, timestamp: data.timestamp }));
+  const chartData = data.toReversed().map((data) => ({
+    value: data[selectedMetric],
+    timestamp: data.timestamp,
+  }));
 
   function handleChangeTimeRange(range: string) {
     if (typeof window === 'undefined') return;
@@ -51,35 +83,47 @@ export function ChartAreaInteractive({ data }: { data: AirQuality[] }) {
     router.push(url.toString(), { scroll: false });
   }
 
+  const chartConfig = {
+    value: {
+      label: `${metrics[selectedMetric].heading.widescreen}:\u00A0\u00A0`,
+      color: 'var(--primary)',
+    },
+  } satisfies ChartConfig;
+
   return (
-    <Card className='@container/card mx-4 min-h-[335px] lg:mx-4'>
-      <CardHeader>
-        <CardTitle className='hidden @[540px]/card:block'>Poluentes Orgânicos Voláteis</CardTitle>
-        <CardTitle className='@[540px]/card:hidden'>Poluentes Orgânicos</CardTitle>
-        <CardDescription>
-          <span className='hidden @[540px]/card:block'>{currentDescription.long}</span>
-          <span className='@[540px]/card:hidden'>{currentDescription.short}</span>
-        </CardDescription>
-        <CardAction>
-          <ToggleGroup
-            type='single'
-            value={timeRange}
-            onValueChange={(value) => {
-              if (value) handleChangeTimeRange(value);
-            }}
-            variant='outline'
-            className='hidden *:data-[slot=toggle-group-item]:!px-4 @[767px]/card:flex'
-          >
-            <ToggleGroupItem className='cursor-pointer' value='14d'>
-              Últimos 14 dias
-            </ToggleGroupItem>
-            <ToggleGroupItem className='cursor-pointer' value='7d'>
-              Últimos 7 dias
-            </ToggleGroupItem>
-            <ToggleGroupItem className='cursor-pointer' value='1d'>
-              Últimas 24 horas
-            </ToggleGroupItem>
-          </ToggleGroup>
+    <Card className='@container/card mx-4 min-h-[335px] gap-2 lg:mx-4'>
+      <CardHeader className='flex justify-between gap-2 max-sm:gap-4 min-md:items-center'>
+        <div className='flex flex-col gap-1'>
+          <CardTitle className='hidden @[540px]/card:block'>
+            {metrics[selectedMetric].heading.widescreen}
+          </CardTitle>
+          <CardTitle className='@[540px]/card:hidden'>{metrics[selectedMetric].heading.mobile}</CardTitle>
+          <CardDescription>
+            <span className='hidden @[540px]/card:block'>
+              {metrics[selectedMetric].description.long} {timeRangeDescriptions[timeRange].long}
+            </span>
+            <span className='@[540px]/card:hidden'>
+              {metrics[selectedMetric].description.short} {timeRangeDescriptions[timeRange].short}
+            </span>
+          </CardDescription>
+        </div>
+        <CardAction className='flex gap-2 max-md:flex-col max-sm:items-end'>
+          <Select value={selectedMetric} onValueChange={(value) => setSelectedMetric(value as Metric)}>
+            <SelectTrigger
+              className='w-40 cursor-pointer max-md:order-1 max-sm:w-fit'
+              size='sm'
+              aria-label='Selecione métrica'
+            >
+              <SelectValue placeholder='Selecione métrica' />
+            </SelectTrigger>
+            <SelectContent className='rounded-xl'>
+              {Object.entries(metrics).map(([key, { description }]) => (
+                <SelectItem key={key} value={key} className='cursor-pointer rounded-lg'>
+                  {description.short}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
           <Select
             value={timeRange}
             onValueChange={(value) => {
@@ -87,7 +131,7 @@ export function ChartAreaInteractive({ data }: { data: AirQuality[] }) {
             }}
           >
             <SelectTrigger
-              className='flex w-40 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate @[767px]/card:hidden'
+              className='w-40 cursor-pointer max-sm:w-fit'
               size='sm'
               aria-label='Selecione o período'
             >
@@ -114,8 +158,8 @@ export function ChartAreaInteractive({ data }: { data: AirQuality[] }) {
               <BarChart2 className='text-muted-foreground/70 h-12 w-12' />
               <h3 className='text-lg font-semibold'>Nenhum dado disponível</h3>
               <p className='text-muted-foreground max-w-sm text-sm'>
-                Não há dados de poluentes orgânicos para o período selecionado. Ajuste os filtros ou escolha
-                outro intervalo de tempo.
+                Não há dados para o período selecionado. Ajuste os filtros ou escolha outro intervalo de
+                tempo.
               </p>
             </div>
           ) : (
@@ -138,7 +182,7 @@ export function ChartAreaInteractive({ data }: { data: AirQuality[] }) {
                   return date.toLocaleDateString('pt-BR', { month: 'short', day: 'numeric' });
                 }}
               />
-              <YAxis tickLine={false} axisLine={false} tickMargin={8} width={30} />
+              <YAxis tickLine={false} axisLine={false} tickMargin={8} width={40} />
               <ChartTooltip
                 defaultIndex={isMobile ? -1 : 10}
                 content={
--- apps/frontend/src/components/dashboard/dashboard-header.tsx
+++ apps/frontend/src/components/dashboard/dashboard-header.tsx
@@ -1,19 +1,33 @@
+'use client';
+
 import type { AirQuality } from '@/lib/schemas/air-quality.schema';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { Separator } from '@/components/ui/separator';
+import { useMobile } from '@/hooks/shared/useMobile.hook';
 
 export function DashboardHeader({ latestEntry }: { latestEntry: AirQuality | null }) {
-  let formatted: string | null = null;
+  let displayText: string | null = null;
+  const isMobile = useMobile();
 
   if (latestEntry) {
-    formatted = new Date(latestEntry.timestamp).toLocaleDateString('pt-BR', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      timeZone: 'America/Sao_Paulo',
-    });
+    const date = new Date(latestEntry.timestamp);
+
+    displayText = isMobile
+      ? date.toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          hour: '2-digit',
+          minute: '2-digit',
+          timeZone: 'America/Sao_Paulo',
+        })
+      : date.toLocaleDateString('pt-BR', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          timeZone: 'America/Sao_Paulo',
+        });
   }
 
   return (
@@ -21,12 +35,18 @@ export function DashboardHeader({ latestEntry }: { latestEntry: AirQuality | nul
       <div className='flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6'>
         <SidebarTrigger className='-ml-1 cursor-pointer' />
         <Separator orientation='vertical' className='mx-2 data-[orientation=vertical]:h-4' />
-        <div className='ml-auto flex items-center gap-2'>
+        <div className='flex w-full items-center justify-between gap-2'>
           <span className='text-base font-medium max-sm:hidden'>Monitoramento Ambiental</span>
-          {formatted ? (
-            <p className='text-xs'>(Última Atualização: {formatted})</p>
+          {displayText ? (
+            <div className='ml-auto'>
+              <p className='hidden text-xs max-sm:block'>(Atual: {displayText})</p>
+              <p className='text-xs max-sm:hidden'>(Última Atualização: {displayText})</p>
+            </div>
           ) : (
-            <p className='text-muted-foreground text-xs'>(Nenhuma atualização disponível)</p>
+            <div className='ml-auto'>
+              <p className='text-muted-foreground hidden text-xs max-sm:block'>(Sem atualização)</p>
+              <p className='text-muted-foreground text-xs max-sm:hidden'>(Nenhuma atualização disponível)</p>
+            </div>
           )}
         </div>
       </div>
